14329c437a2a3339d65f0f6cbfc6f15da3fd12d4
BarbarianTora <motorolllka@gmail.com>
2018-04-06 16:10:28 +0300

    +ImageTarget (Vuforia)

---
+++ Assets/Scripts/ImageTargetManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImageTargetManager : DefaultTrackableEventHandler
+
+{
+
+	[SerializeField]
+	private List<GameObject> _listOfManagmentObjects = new List<GameObject>();
+
+	protected override void Start()
+	{
+
+		base.Start ();
+		StateManagmentObjectsSwitcher (false);
+	}
+		
+	protected override void OnTrackingFound()
+	{
+		StateManagmentObjectsSwitcher (true);
+	}
+		
+	protected override void OnTrackingLost()
+	{
+		StateManagmentObjectsSwitcher (false);
+	}
+
+	private void StateManagmentObjectsSwitcher(bool state)
+	{
+		foreach (GameObject managmentObject in _listOfManagmentObjects)
+			{
+				if(managmentObject != null)
+				managmentObject.SetActive (state);
+			}
+	}
+		
+//
+//	protected override void OnDestroy()
+//	{
+//		base.OnDestroy ();
+//	}
+//
+}
---
+++ Assets/Scripts/TargetCheck.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetCheck : DefaultTrackableEventHandler {
+
+	public GameObject[] blocks = null;
+
+	public void OnTargetFound()
+	{
+		base.OnTrackingFound ();
+		foreach (GameObject obj in blocks) 
+		{
+			obj.SetActive(true); 
+
+		}
+
+	}
+	public void OnTargetLost()
+	{
+		foreach (GameObject obj in blocks) 
+		{
+			obj.SetActive(false); 
+		}
+	}
+}
+
+
