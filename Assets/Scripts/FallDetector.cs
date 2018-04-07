cd63d5ea9023f7d534e4c0eddb30744b3b87335b
BarbarianTora <motorolllka@gmail.com>
2018-04-07 12:35:46 +0300

    +FallDetector

---
+++ Assets/Scripts/FallDetector.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallDetector : MonoBehaviour {
+
+	private const float _xMin = -3.2f;
+	private const float _zMin = -2.96f;
+	private const float _xMax = 5.5f;
+	private const float _zMax = 4.74f;
+	private const float _spawnY =  4f;
+
+	void Start () 
+	{
+		
+	}
+	
+	void Update () 
+	{
+		
+	}
+
+	void OnTriggerEnter (Collider col)
+	{
+		if (col.tag == "MO") 
+		{
+			col.transform.position = new Vector3 (Random.Range (_xMin, _xMax),
+												  _spawnY, 
+												  Random.Range (_zMin, _zMax));
+		}
+	}
+}
